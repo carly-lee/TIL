1ee596a0ec4c86729dedab12a10c126b85db73d0
Carly Lee <carly.l86@gmail.com>
2017-08-22 01:32:12 +0900

    Add a bit more notes and example codes

---
+++ Functional-progamming/#01/box3.js
@@ -0,0 +1,9 @@
+const nextCharForNumberString = str =>
+  [str]
+  .map(s => s.trim())
+  .map(s => parseInt(s))
+  .map(i => i + 1)
+  .map(i => String.fromCharCode(i)) // [ 'A' ]
+
+const result = nextCharForNumberString('  64 ')
+console.log(result)
